ba4bbd6aa946f04479b07860d65c11fbf2b34c72
Celeste Morton <creneemorton@gmail.com>
2020-02-28 09:15:06 -0800

    add guitar chords display option for react

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react
 import { ScrollView } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
 
+
 import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
