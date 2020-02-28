e17442894e7f758adeed3a468d9241691a09b4fa
Celeste Morton <creneemorton@gmail.com>
2020-02-28 11:38:57 -0800

    add different navbar to app.js with three main components

--- App.js
+++ App.js
@@ -9,55 +9,120 @@ import { createStackNavigator } from '@react-navigation/stack';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
 
+import { Text } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+function FeedScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed!</Text>
+    </View>
+  );
+}
+
+function NotificationsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      tabBarOptions={{
+        activeTintColor: 'white',
+        labelStyle: { fontSize: 20 },
+        style: { backgroundColor: '#048B78' },
+      }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={FeedScreen}
+        options={{ tabBarLabel: 'Home' }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationsScreen}
+        options={{ tabBarLabel: 'Songs' }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ tabBarLabel: 'My Tabs' }}
+      />
+    </Tab.Navigator>
+  );
+}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
+
 const Stack = createStackNavigator();
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
+// export default function App(props) {
+//   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+//   const [initialNavigationState, setInitialNavigationState] = React.useState();
+//   const containerRef = React.useRef();
+//   const { getInitialState } = useLinking(containerRef);
 
   // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
+//   React.useEffect(() => {
+//     async function loadResourcesAndDataAsync() {
+//       try {
+//         SplashScreen.preventAutoHide();
 
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
+//         // Load our initial navigation state
+//         setInitialNavigationState(await getInitialState());
 
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
+//         // Load fonts
+//         await Font.loadAsync({
+//           ...Ionicons.font,
+//           'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+//         });
+//       } catch (e) {
+//         // We might want to provide this error information to an error reporting service
+//         console.warn(e);
+//       } finally {
+//         setLoadingComplete(true);
+//         SplashScreen.hide();
+//       }
+//     }
 
-    loadResourcesAndDataAsync();
-  }, []);
+//     loadResourcesAndDataAsync();
+//   }, []);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
-}
+//   if (!isLoadingComplete && !props.skipLoadingScreen) {
+//     return null;
+//   } else {
+//     return (
+//       <View style={styles.container}>
+//         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+//         <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+//           <Stack.Navigator>
+//             <Stack.Screen name="Root" component={BottomTabNavigator} />
+//           </Stack.Navigator>
+//         </NavigationContainer>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
