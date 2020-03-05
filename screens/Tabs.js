39c150368c1c8c287371410b50d5b775ec8ffa97
Celeste Morton <creneemorton@gmail.com>
2020-03-05 09:37:11 -0800

    add material ui for react native

---
+++ screens/Tabs.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import ListItem from '@material-ui/core/ListItem';
+import StarRoundedIcon from '@material-ui/icons/StarRounded';
+
+
+const useStyles = makeStyles(theme => ({
+	listItem: {
+		[theme.breakpoints.down('md')]: {
+			padding: '8px',
+		},
+	},
+	star: {
+		width: 'calc(15px + 1vmin)',
+		minWidth: '28px',
+		color: '#FFDF00',
+		marginLeft: '50px'
+	},
+	link: {
+		marginTop: '-0.5vh',
+		'&:hover': {
+			color: '#770',
+		}
+	},
+	type: {
+		paddingLeft: '5px',
+		[theme.breakpoints.down('md')]: {
+			padding: '6px',
+		},
+	}
+}));
+
+const Tab = ({ id, artist, title }) => {
+	const classes = useStyles();
+	return (
+		<ListItem className={classes.listItem}>
+			<Link
+				// target="_blank"
+				underline="none"
+				className={classes.link}
+				rel="noopener noreferrer"
+				href={`http://www.songsterr.com/a/wa/bestMatchForQueryString?s=${title}&a=${artist}`}>{artist} - {title}
+			</Link>
+		
+			<StarRoundedIcon className={classes.star} />
+		</ListItem>
+	);
+}
+
+export default Tab;
