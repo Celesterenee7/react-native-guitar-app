3b6699b6cbd098bb322c702f09e69a83770e1442
Celeste Morton <creneemorton@gmail.com>
2020-03-05 11:48:55 -0800

    add bottomnav to nav folder and clean out app file

--- App.js
+++ App.js
@@ -1,96 +1,12 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import SearchBarNavigator from './navigation/SearchBarNavigator';
-import { MaterialCommunityIcons } from 'react-native-vector-icons';
-import { Feather } from 'react-native-vector-icons';
+import BottomNavigation from './navigation/BottomNavigation'
 
-
-
-
-function HomeFeed() {
-  return (
-    
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-      <Text>Welcome!</Text>
-    </View>
-  );
-}
-
-function FavTabs() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-      <Text>Favorite Tabs</Text>
-    </View>
-  );
-}
-
-
-function Search() {
-  return (
-    <View>
-      <SearchBarNavigator />
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="HomeFeed"
-      tabBarOptions={{
-        activeTintColor: '#048B78',
-        activeBackgroundColor: '#83FAE7',
-      }}
-    >
-      <Tab.Screen
-        name="HomeFeed"
-        component={HomeFeed}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="FavTabs"
-        component={FavTabs}
-        options={{
-          tabBarLabel: 'My Tabs',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="guitar-acoustic" color={color} size={size} />
-          ),
-        }}
-      />
-         <Tab.Screen
-        name="Search"
-        component={Search}
-        options={{
-          tabBarLabel: 'Search',
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="search" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
-
-
-export default function App() {
-  
-  return (
-    <NavigationContainer>
-     
-      <MyTabs />
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <BottomNavigation />
+    );
+  }
 }
 
 
--- navigation/SearchBarNavigator.js
+++ navigation/SearchBarNavigator.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import { Toolbar } from 'react-native-material-ui';
@@ -17,11 +15,6 @@ const useStyles = makeStyles(theme => ({
 		textAlign: 'center',
 		color: 'orange',
 		fontSize: '30px'
-	},
-	button: {
-		margin: '1.5vh',
-		color: 'white',
-		fontSize: '7em'
 	},
 	input: {
 		background: 'white',
@@ -41,11 +34,10 @@ const Searchbox = ({ searchChange, searchTabs }) => {
   }
   
 	return (
-		<Card className={classes.root} square >
 			<Toolbar style={{ container: { backgroundColor: '#57C6AF' }}}
       background='#57C6AF'
       leftElement="menu"
-      centerElement="Searchable"
+
       searchable={{
         autoFocus: true,
         placeholder: 'Search',
@@ -72,7 +64,6 @@ const Searchbox = ({ searchChange, searchTabs }) => {
 					)
 				}}
 			/>
-		</Card>
 	);
 }
 
