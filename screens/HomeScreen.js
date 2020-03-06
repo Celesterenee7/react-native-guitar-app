95e37d2b9efb0f12a41d1638b15e4aab36ac0e0c
Celeste Morton <creneemorton@gmail.com>
2020-03-05 17:07:16 -0800

    add 100 percent width to flatlist

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -18,15 +18,11 @@ export default class ListWithSearchView extends Component {
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       dataSource: ds.cloneWithRows([
-        {icon:"https://png.icons8.com/user-folder/color/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"},
-        {icon:"https://png.icons8.com/find-user-male/color/100/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"}, 
-        {icon:"https://png.icons8.com/desktop/office/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"}, 
-        {icon:"https://png.icons8.com/firefox/color/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"}, 
-        {icon:"https://png.icons8.com/pc-on-desk/color/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"}, 
-        {icon:"https://png.icons8.com/mandriva/color/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"}, 
-        {icon:"https://png.icons8.com/microsoft-access/color/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"}, 
-        {icon:"https://png.icons8.com/user-folder/office/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"},
-        {icon:"https://png.icons8.com/facebook-messenger/color/40/2ecc71", description: "Lorem ipsum dolor sit amet, indu consectetur adipiscing elit"},
+        {icon:"https://png.icons8.com/user-folder/color/40/2ecc71", description: "The Beatles - Blackbird"},
+        {icon:"https://png.icons8.com/find-user-male/color/100/2ecc71", description: "Johnny Cash - Walk The Line"}, 
+        {icon:"https://png.icons8.com/desktop/office/40/2ecc71", description: "Joni Mitchell - A Case of You"}, 
+        {icon:"https://png.icons8.com/firefox/color/40/2ecc71", description: "Luke Combs - Hurricane"}, 
+        {icon:"https://png.icons8.com/pc-on-desk/color/40/2ecc71", description: "Taylor Swift - Call it What You Want"}, 
       ]),
     };
   }
@@ -38,13 +34,6 @@ export default class ListWithSearchView extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.formContent}>
-
-          <TouchableHighlight style={styles.saveButton} onPress={() => this.clickEventListener('search')}>
-            <Image style={[styles.icon, styles.iconBtnSearch]} source={{uri: 'https://png.icons8.com/search/androidL/100/ffffff'}}/>
-          </TouchableHighlight>
-        </View>
-
         <ListView style={styles.notificationList} enableEmptySections={true}
           dataSource={this.state.dataSource}
           renderRow={(notification) => {
@@ -63,23 +52,24 @@ export default class ListWithSearchView extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
     flex: 1,
     backgroundColor: '#EBEBEB',
   },
   formContent:{
     flexDirection: 'row',
-    marginTop:30,
+    marginTop: 50,
   },
   inputContainer: {
       borderBottomColor: '#F5FCFF',
       backgroundColor: '#FFFFFF',
-      borderRadius:30,
+      borderRadius:20,
       borderBottomWidth: 1,
-      height:45,
+      height:20,
       flexDirection: 'row',
       alignItems:'center',
       flex:1,
-      margin:10,
+      margin:20,
   },
   icon:{
     width:30,
@@ -98,22 +88,9 @@ const styles = StyleSheet.create({
     marginLeft:15,
     justifyContent: 'center'
   },
-  saveButton: {
-    height:45,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin:10,
-    width:70,
-    alignSelf: 'flex-end',
-    backgroundColor: '#40E0D0',
-    borderRadius:30,
-  },
-  saveButtonText: {
-    color: 'white',
-  },
   notificationList:{
     marginTop:20,
-    padding:10,
+    padding:50,
   },
   notificationBox: {
     padding:20,
