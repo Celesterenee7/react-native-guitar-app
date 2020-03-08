ddea7eb41063a8932f3a19c52e33812b4b2825dc
Celeste Morton <creneemorton@gmail.com>
2020-03-08 16:49:59 -0700

    add react router for app.js navigation

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import * as React from 'react';
+import {SafeAreaView} from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
+import { t } from  'react-native-tailwindcss';
+
+// Navigation
 import BottomNavigation from './navigation/BottomNavigation';
-import { Button, View, Text } from 'react-native';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import FavTabsScreen from './screens/FavTabsScreen';
+import SearchScreen from './screens/SearchScreen';
 
 export default class App extends React.Component {
   render() {
