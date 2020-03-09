bd032fa5be01c917d3132b249a916f689c29a34a
Celeste Morton <creneemorton@gmail.com>
2020-03-08 18:16:47 -0700

    add changes to app.js and then revert to original layout

--- App.js
+++ App.js
@@ -1,33 +1,89 @@
 import * as React from 'react';
-import {SafeAreaView} from 'react-native';
-import {NativeRouter, Switch, Route} from 'react-router-native';
-import { t } from  'react-native-tailwindcss';
-
-// Navigation
-import BottomNavigation from './navigation/BottomNavigation';
-
-// Screens
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SearchScreen from './screens/SearchScreen';
 import HomeScreen from './screens/HomeScreen';
 import FavTabsScreen from './screens/FavTabsScreen';
-import SearchScreen from './screens/SearchScreen';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
+import { Feather } from 'react-native-vector-icons';
 
-function App() {
+
+function HomeFeed() {
   return (
-    <SafeAreaView style={[t.bgGray100, t.flex, t.hFull , t.wFull]}>
-      <Switch>
-        <Route exact path='/' component={HomeScreen}/>
-        <Route exact path='/favtabs' component={FavTabsScreen}/>
-        <Route exact path='/search' component={SearchScreen}/>
-      </Switch>
-      <BottomNavigation />
-    </SafeAreaView>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <HomeScreen />
+    </View>
   );
 }
 
-export default () => (
-  <NativeRouter>
-    <App/>
-  </NativeRouter>
-)
+function FavTabs() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <FavTabsScreen/>
+    </View>
+  );
+}
 
 
+function Search() {
+  return (
+    <View>
+      <SearchScreen/>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeFeed"
+      tabBarOptions={{
+        activeTintColor: '#048B78',
+        activeBackgroundColor: '#83FAE7',
+      }}
+    >
+      <Tab.Screen
+        name="HomeFeed"
+        component={HomeFeed}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="FavTabs"
+        component={FavTabs}
+        options={{
+          tabBarLabel: 'My Tabs',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="guitar-acoustic" color={color} size={size} />
+          ),
+        }}
+      />
+         <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
