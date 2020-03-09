eef8ad9694732203b5cc80643d70199ee07b183a
Celeste Morton <creneemorton@gmail.com>
2020-03-08 20:43:29 -0700

    add successful image to flexbox on fav tabs screen

--- screens/FavTabsScreen.js
+++ screens/FavTabsScreen.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Dimensions } from 'react-native'
+import React, { Component } from 'react'
+import { View, Image } from 'react-native'
 
-export default class FavTabsScreen extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View style ={styles.container}>
-      <ImageBackground
-           source={require("./images/luke.jpg")}
-           style={styles.background}>
-
-         {/* It is in this part I have a problem */}
-         <View style ={styles.imageContainer}>
-           <Image resizeMode='contain'
-                      style ={{width: Dimensions.get('window').width/2, height: Dimensions.get('window').width/2}}
-                      source={require("./images/luke.jpg")}/>
-           <Image resizeMode='contain'
-                  style ={{width: Dimensions.get('window').width/2, height: Dimensions.get('window').width/2}}
-                  source={require("./images/luke.jpg")}/>
-         </View>
-     </ImageBackground>
-   </View>
-     );
+      // Try setting `flexDirection` to `column`.
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <Image source = {{uri:'https://d3gnjjq3mhphne.cloudfront.net/uploads/image/post/022/784/22784/header_vstory/22784_109874_608_59330275_2183135961767382_1434277828550983680_n.jpg'}}
+   style = {{ width: 200, height: 200 }}
+   />
+      </View>
+    );
   }
-}
+};
+
+
+
+// const ImagesExample = () => (
+//    <Image source = {{uri:'https://d3gnjjq3mhphne.cloudfront.net/uploads/image/post/022/784/22784/header_vstory/22784_109874_608_59330275_2183135961767382_1434277828550983680_n.jpg'}}
+//    style = {{ width: 200, height: 200 }}
+//    />
+// )
+// export default ImagesExample
