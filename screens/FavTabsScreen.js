792256ff549fddac4230e9d789003b433d4fb596
Celeste Morton <creneemorton@gmail.com>
2020-03-08 21:11:46 -0700

    add stylesheet for fav tabs container

--- screens/FavTabsScreen.js
+++ screens/FavTabsScreen.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react'
-import { View, Image } from 'react-native'
+import React, { Component } from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import HomeScreen from './HomeScreen';
+import Constants from 'expo-constants';
 
-export default class FlexDirectionBasics extends Component {
+export default class FavTabs extends Component {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View>
+     <HomeScreen/>
+      <View style={styles.container}>
              <Image source = {{uri:'https://townsquare.media/site/295/files/2015/12/BeatlesForSale_1.jpg?w=980&q=75'}}
-   style = {{ width: 500, height: 500 }}
+   style = {{ width: 400, height: 400 }}
    />
         <Image source = {{uri:'https://d5wt70d4gnm1t.cloudfront.net/media/a-s/artworks/globe-photo-agency-3/48183-262458029854/globe-photo-agency-3-johnny-cash-smoking-and-playing-guitar-800x800.jpg'}}
-   style = {{ width: 500, height: 500 }}
+   style = {{ width: 400, height: 400 }}
    />
         <Image source = {{uri:'https://d3gnjjq3mhphne.cloudfront.net/uploads/image/post/022/784/22784/header_vstory/22784_109874_608_59330275_2183135961767382_1434277828550983680_n.jpg'}}
-   style = {{ width: 500, height: 500 }}
+   style = {{ width: 400, height: 400 }}
    />
+      </View>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flex: 1,
+    flexDirection: 'row',
+    // marginTop: Constants.statusBarHeight,
+    marginTop: 50,
+    marginBottom: 50
+  }
+});
+
