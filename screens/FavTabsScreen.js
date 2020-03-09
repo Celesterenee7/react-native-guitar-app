78ef1e761bffb52906c9118663a4402c2d38015b
Celeste Morton <creneemorton@gmail.com>
2020-03-08 20:30:54 -0700

    attempt to add image grid to fav tabs

--- screens/FavTabsScreen.js
+++ screens/FavTabsScreen.js
@@ -1,91 +1,26 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  TouchableOpacity,
-  FlatList,
-  StyleSheet,
-  Text,
-} from 'react-native';
-import Constants from 'expo-constants';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Dimensions } from 'react-native'
 
+export default class FavTabsScreen extends Component {
+  render() {
+    return (
+      <View style ={styles.container}>
+      <ImageBackground
+           source={require("./images/luke.jpg")}
+           style={styles.background}>
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'The Beatles',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Johnny Cash',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Luke Combs',
-  },
-];
-
-function Item({ id, title, selected, onSelect }) {
-  return (
-    <TouchableOpacity
-      onPress={() => onSelect(id)}
-      style={[
-        styles.item,
-        { backgroundColor: selected ? 'rgb(131, 250, 231)' : 'white' },
-      ]}
-    >
-      <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
-  );
+         {/* It is in this part I have a problem */}
+         <View style ={styles.imageContainer}>
+           <Image resizeMode='contain'
+                      style ={{width: Dimensions.get('window').width/2, height: Dimensions.get('window').width/2}}
+                      source={require("./images/luke.jpg")}/>
+           <Image resizeMode='contain'
+                  style ={{width: Dimensions.get('window').width/2, height: Dimensions.get('window').width/2}}
+                  source={require("./images/luke.jpg")}/>
+         </View>
+     </ImageBackground>
+   </View>
+     );
+  }
 }
-
-export default function App() {
-  const [selected, setSelected] = React.useState(new Map());
-
-  const onSelect = React.useCallback(
-    id => {
-      const newSelected = new Map(selected);
-      newSelected.set(id, !selected.get(id));
-
-      setSelected(newSelected);
-    },
-    [selected],
-  );
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({ item }) => (
-          <Item
-            id={item.id}
-            title={item.title}
-            selected={!!selected.get(item.id)}
-            onSelect={onSelect}
-          />
-        )}
-        keyExtractor={item => item.id}
-        extraData={selected}
-      />
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-    // marginTop: 50,
-  },
-  item: {
-    backgroundColor: '#EBEBEB',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderBottomColor: '#DBDBDA',
-    borderBottomWidth: 1,
-  },
-  title: {
-    fontSize: 22,
-  },
-});
