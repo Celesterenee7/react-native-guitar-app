84a6b0fe5dba1952da2425f9a00375a444e7cdbc
Celeste Morton <creneemorton@gmail.com>
2020-03-08 20:52:01 -0700

    add flexbox of musician photos to fav tabs

--- screens/FavTabsScreen.js
+++ screens/FavTabsScreen.js
@@ -6,21 +6,16 @@ export default class FlexDirectionBasics extends Component {
     return (
       // Try setting `flexDirection` to `column`.
       <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+             <Image source = {{uri:'https://townsquare.media/site/295/files/2015/12/BeatlesForSale_1.jpg?w=980&q=75'}}
+   style = {{ width: 500, height: 500 }}
+   />
+        <Image source = {{uri:'https://d5wt70d4gnm1t.cloudfront.net/media/a-s/artworks/globe-photo-agency-3/48183-262458029854/globe-photo-agency-3-johnny-cash-smoking-and-playing-guitar-800x800.jpg'}}
+   style = {{ width: 500, height: 500 }}
+   />
         <Image source = {{uri:'https://d3gnjjq3mhphne.cloudfront.net/uploads/image/post/022/784/22784/header_vstory/22784_109874_608_59330275_2183135961767382_1434277828550983680_n.jpg'}}
-   style = {{ width: 200, height: 200 }}
+   style = {{ width: 500, height: 500 }}
    />
       </View>
     );
   }
 };
-
-
-
-// const ImagesExample = () => (
-//    <Image source = {{uri:'https://d3gnjjq3mhphne.cloudfront.net/uploads/image/post/022/784/22784/header_vstory/22784_109874_608_59330275_2183135961767382_1434277828550983680_n.jpg'}}
-//    style = {{ width: 200, height: 200 }}
-//    />
-// )
-// export default ImagesExample
