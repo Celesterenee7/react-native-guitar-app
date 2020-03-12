5792f3de04b60e3faf6d5f8f5d1587adc7c2c739
Celeste Morton <creneemorton@gmail.com>
2020-03-12 15:15:59 -0700

    add header access control for cors

--- app/actions/songsActions.js
+++ app/actions/songsActions.js
@@ -35,6 +35,10 @@ export function createSong(song) {
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+          "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+          "Access-Control-Allow-Methods": "POST, OPTIONS"
         },
         body: JSON.stringify({
           song
